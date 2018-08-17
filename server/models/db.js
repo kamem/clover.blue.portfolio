038fc90676b3f93aa63be49d26210a2dd0e4750b
kamem <develo0@gmail.com>
2018-08-18 00:03:41 +0900

    no message

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,7 @@
 import settings from '../settings'
 import mongo from 'mongoose'
 
-mongo.connect( process.env.NODE_ENV === 'production' ? 'mongodb://heroku_h13ngxp2:4cr1mbcvh2btaip4uvtqbc542m@ds123532.mlab.com:23532/heroku_h13ngxp2' : `mongodb://localhost/${settings.dbName}`)
+mongo.connect( process.env.NODE_ENV === 'production' ? 'mongodb://heroku_h13ngxp2:4cr1mbcvh2btaip4uvtqbc542m@ds123532.mlab.com:23532/heroku_h13ngxp2' : `mongodb://localhost/${settings.dbName}`, { useNewUrlParser: true })
 const db = mongo.connection
 
 db.on('error', console.error.bind(console, 'connection error:'))
